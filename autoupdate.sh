1ad5c58f0d356cdc704e4626628fa63924a852db
Vo Que Son <sonvoque@gmail.com>
2017-04-18 21:34:17 +0700

    Add files via upload

---
+++ autoupdate.sh
@@ -0,0 +1,7 @@
+sudo chown user gw
+sudo rm -r gw
+sudo git clone https://github.com/sonvoque/sls_gw
+sudo mv sls_gw gw
+cd gw
+sudo chmod +x automain.sh
+sudo gcc -o main main.c $(mysql_config --libs --cflags)
